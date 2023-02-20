dc941dd0cbace33ee98eadd0073517c6b7f1c5bc
MarinaSusanne <susanne.hagen@live.nl>
2023-02-20 19:53:12 +0100

    semi af

--- src/main/java/Controllers/TelevisionController.java
+++ src/main/java/Controllers/TelevisionController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @RestController
 public class TelevisionController {
 
@@ -14,30 +16,44 @@ public class TelevisionController {
         }
 
     @GetMapping("television/{id}")
-    public ResponseEntity<String> getTVWidth (@PathVariable int id){
+    public ResponseEntity<String> getOneTv (@PathVariable int id){
         if (id == 5){
-            throw new RecordNotFoundException("Ik snap het bijna!");
+            throw new RecordNotFoundException("een message");
         }
         return  new ResponseEntity<> ("television" + id , HttpStatus.OK);
+
     }
+
+    // als het een Array was..wat veel logischer was in deze opdracht:
+    // @GetMapping ("television/{id}")
+    //   public ResponseEntity <Television> getOneTelevision (@PathVariable int id){
+    //   if (id>= 0 && id <= telelevision.size()) {
+    //      return new ResponseEntity <> (televisions.get(id), Httpstatus.ok);
+    //       }
+    //      else {
+    //      throw new RecordNotFoundException("ID not available");
+
+
 
     @PutMapping ("television/{id}")
     public ResponseEntity<String> updateTVList (@PathVariable int id, @RequestBody String name){
         return new ResponseEntity<>("television" + id, HttpStatus.NO_CONTENT);
-        //moet het niet name zijn?
     }
+
+
 
     @PostMapping("television")
     public ResponseEntity <String> addTVList (@RequestBody String television){
         return ResponseEntity.created(null).body("television");
     }
 
-    @DeleteMapping ("Televisio/{id}")
+
+
+    @DeleteMapping ("Television/{id}")
     public ResponseEntity<String> deleteById (@PathVariable int id){
         return ResponseEntity.noContent().build();
     }
 
 }
 
-//haha
 
